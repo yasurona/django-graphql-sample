c81265b4113add6875f23706a44f0c78ac3f5696
yasurona <seiju4@gmail.com>
2022-10-16 02:01:48 +0900

    add: cors

--- src/sampleproject/settings.py
+++ src/sampleproject/settings.py
@@ -51,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    os.getenv('FRONT_URL'),
 ]
 
 ROOT_URLCONF = 'sampleproject.urls'
